7d725af913789a5b63c73b8717424a46a4777a01
H.kevin <mh.ha@artdefinance.io>
2023-02-23 11:51:07 +0900

    feat : scope

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,26 +1,22 @@
-// variable
+// scope
 fn main() {
-    let _a = 10; // immutable variable
-    //_a = 5;    // error, 변경 불가
+    let x = 5;
+    {
+        let y = 99;
+        println!("{}, {}", x, y);
+    }
+    //println!("{}, {}", x, y); // error, y undefined
 
-    let mut _b = 10; // mutable variable
-    _b = 5;          // 변경 가능
+    {
+        let x = 99;
+        println!("{}", x);
+    }
+    println!("{}", x);
 
-    // 상수 선언
-    // 1. let -> const
-    // 2. use screaming-snake-case 
-    // 3. type annotation is required
-    // 4. value must be a constant expression 
-    // that can be determined at compile tiem
+    // shadow 
+    let mut x = 5; // x is mutable
+    let x = x; // x is now immutable
 
-    // ex.
-    // let _a = random();
-    // 1. const _a = random();
-    // 2. const A_VALUE = random();
-    // 3. const A_VALUE: i32 = random();
-    // 4. const A_VALUE: i32 = 10;
-
-    // why use const variable 
-    // 1. reusalbe, global variable
-    // 2. const 값은 컴파일 타임에 인라인 되기 때문에 빠름
+    // let meme = "Meme";
+    // let meme = make_image(meme); // meme은 "MeMe"로 시작하여 이미가 됨.
 }
