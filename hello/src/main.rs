61da5d2de8e0e2934fe87fdbc74b6d6dcfe0867a
H.kevin <mh.ha@artdefinance.io>
2023-02-23 12:02:45 +0900

    feat : memory safety

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,22 +1,24 @@
-// scope
+// memory safety
+// Rust는 컴파일 타임에 Memory safety를 보장함 
 fn main() {
-    let x = 5;
-    {
-        let y = 99;
-        println!("{}, {}", x, y);
+    let enigma: i32;        // enigma 선언은 했으나, 초기화 x
+    println!("{}", enigma); // 컴파일 타임에 값을 알 수 없으므로, 에러
+                            // c언어에서는 에러 X -> 쓰레기값 출력
+
+    let enigma2: i32;
+    if true {               // 컴파일러는 컴파일 타임에 조건 값에 대해 추론X
+                            // 조건이 어떻든, 조건부 평가는 런타임에 처리되므로
+                            // 컴파일러는 enigma2가 사용되기 전에 초기화되도록 보장
+        enigma2 = 42;
     }
-    //println!("{}, {}", x, y); // error, y undefined
+    println!("{}", enigma2); // 에러
 
-    {
-        let x = 99;
-        println!("{}", x);
+    let enigma3: i32;
+    if true {
+        enigma3 = 42;
+    } else {
+        enigma3 = 1;
     }
-    println!("{}", x);
-
-    // shadow 
-    let mut x = 5; // x is mutable
-    let x = x; // x is now immutable
-
-    // let meme = "Meme";
-    // let meme = make_image(meme); // meme은 "MeMe"로 시작하여 이미가 됨.
+    println!("{}", enigma3); // 정상동작, 위 조건문을 통해, enigma3이 사용되기전,
+                             // 초기화 된다는 것을 컴파일러가 알 수 있음
 }
