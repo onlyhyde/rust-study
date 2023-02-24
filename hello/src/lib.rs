957c4a66659c10f8e6072d58e4f87461f1f33a0b
H.kevin <mh.ha@artdefinance.io>
2023-02-24 21:37:10 +0900

    feat : Module System

---
+++ hello/src/lib.rs
@@ -0,0 +1,4 @@
+// 다른 곳에서 접근할 수 있도록, pub (public의 의미)을 추가
+pub fn greet() {
+  println!("Hi");
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,24 +1,13 @@
-// Rust Style Guide
-// Function -> snake-case 
-// Function 사용을 위해 먼저 노출될 필요가 없음
-// parameters are always defined "name:type"
-// fn do_stuff(qty: f64)
-// return type은 arrow poining 으로 parameter 뒤에 지정
-// fn do_stuff(qty: f64) -> f64
+// Module
+// package name의 greet 를 사용하겠다는 의미
+// package name은 cargo.toml 에 정의
+use hello::greet;
+use rand::thread_rng;
+use rand::Rng;
 
 fn main() {
-
+    greet();
+    let random = thread_rng().gen_range(0, 100);
+    println!("{}", random);
 }
-
-
-// tail expression
-// 값을 반환하는 약어
-// 블럭 마지막 라인에 세미콜론을 빼면 값으로 반환
-// {return ture;} == {true}
-fn do_stuff(qty: f64, oz: f64) -> f64 {
-    qty * oz
-}
-
-// macro
-// 매크로 이름은 항상 !로 끝난다 
-// println!("{}", qty);
+s
